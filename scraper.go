232ca5ecbc15ba89ba11e143b098cf70e749af27
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-06-22 00:42:16 +0300

    getting timeout error. will try to fix

--- scraper.go
+++ scraper.go
@@ -6,9 +6,15 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type Product struct {
+	url, image, name, price string
+}
+
 func main() {
 	// scraping logic here
 	// gonna use ScrapingCourse.com as a test
+
+	var products []Product
 
 	c := colly.NewCollector()
 	// colly's main entity is Collector
@@ -28,21 +34,32 @@ func main() {
 		// OnResponse() method is called after a response is received
 		fmt.Println("Visited", r.Request.URL)
 	})
-	c.OnHTML("a", func(e *colly.HTMLElement) {
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
 		// OnHTML() method is called when an HTML element is found
-		// it prints the URL of the page being visited
-		link := e.Attr("href")
-		fmt.Println("Link found:", link)
+		// the first argument is the HTML element to look for
+		// we can see that on the console before we start scraping
+
+		product := Product{}
+
+		// the data of our interest
+		product.url = e.ChildAttr("a", "href")
+		product.image = e.ChildAttr("img", "src")
+		product.name = e.ChildText("h2")
+		product.price = e.ChildText(".price")
+
+		products = append(products, product)
+
 	})
 	c.OnScraped(func(r *colly.Response) {
 		// OnScraped() method is called after the scraping is done
 		fmt.Println("Finished scraping ", r.Request.URL)
 	})
 
-	c.Visit("https://scrapingcourse.com/")
+	c.Visit("https://scrapingcourse.com/ecommerce")
 	// Visit() method makes a GET request to the URL passed as an argument
 	// and starts the scraping process
 	// putting this at the end of the code to start the scraping process
 
 	fmt.Println("Hello, World!")
+	fmt.Println(products)
 }
